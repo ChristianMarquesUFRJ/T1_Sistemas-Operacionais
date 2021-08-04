283ac07ece2c78fc8ce8a9552caa75f7bda63525
Christian Marques <christianmarquesos@poli.ufrj.br>
2021-08-04 17:57:18 -0300

    Implementação da task 2
    Cria um fork filho para executar certos comandos que dependem de uma variável do pai.

--- code/main.cpp
+++ code/main.cpp
@@ -1,19 +1,37 @@
+////////////////////////////////////////////////////////////////////////////
+//  Universidade Federal do Rio de Janeiro
+//  Disciplina: Introdução ao Aprendizado de Máquina - EEL770
+//  Professor: Rodrigo Souza Couto
+//  Desenvolvedor: Chritian Marques de Oliveira Silva
+//  DRE: 117.214.742
+//  Trabalho 1: Inicialização de programas e comunicação entre processos
+////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////
+//  IMPORTAÇÃO DE BIBLIOTECAS E ONFIGURAÇÕES DE AMBIENTE
+////////////////////////////////////////////////////////////////////////////
 #include <iostream>
+#include <string>
 #include <unistd.h>
 #include <cstdlib>
-#include <sys/types.h>
 #include <csignal>
-
-/*
- * Interações com o sistema pelo terminal:
- * kill -s USR1 <pid>     => Chama a task1
- * kill -s USR2 <pid>     => Chama a task2
- * kill -s TERM <pid>     => Finaliza a execução
-*/
-
+#include <sys/types.h>
+#include <sys/wait.h>
 using namespace std; 
 
-/* Facilitação de mensagens printadas na tela */
+// Interações com o sistema pelo terminal:
+// kill -SIGUSR1 <pid>     => Chama a task1
+// kill -SIGUSR2 <pid>     => Chama a task2
+// kill -SIGTERM <pid>     => Finaliza a execução
+
+////////////////////////////////////////////////////////////////////////////
+//  VARIAVEL GLOBAL DE COMANDOS PARA EXECUÇÃO
+////////////////////////////////////////////////////////////////////////////
+int commandToRun;
+
+////////////////////////////////////////////////////////////////////////////
+//  INTERFACE
+////////////////////////////////////////////////////////////////////////////
 void print(string msg){
     cout << msg;
     cout.flush();
@@ -21,30 +39,82 @@ void print(string msg){
 void println(string msg){
     cout << msg << endl;
 }
-
+void printPID(){
+    cout << getpid() << endl;
+}
 void showPID(){
-    pid_t pid = getpid();
     println("---------------------------------");
     print("-> PID desse executavel: ");
-    cout << pid << endl; // Possui um tipo diferente => mais facil imprimir sem converter pra string
+    printPID();
     println("---------------------------------");
 }
 
-void task1 (int sigNumber){
+////////////////////////////////////////////////////////////////////////////
+//  TASK 1
+////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////
+//  TASK 2
+////////////////////////////////////////////////////////////////////////////
+void task2Child(){
+    print("Sou o filho, meu pid: ");
+    printPID();
+
+    // Comando executado caso a variavel contenha 0
+    if (commandToRun == 0){
+        println("Nao ha comando a executar");
+        exit(0);
+    }
+
+    // Comando executado caso a variavel contenha um valor par
+    else if (commandToRun % 2 == 0)
+        execlp("/bin/ping","ping","8.8.8.8","-c","5",NULL);
+
+    // Comando executado caso a variavel contenha um valor impar
+    else if (commandToRun % 2 != 0) 
+        execlp("/bin/ping", "ping", "paris.testdebit.info", "-c", "5", "-i", "2", NULL);
+}
+
+void task2Father(int childPID){
+     // Espera especificamente o processo do filho finalizar
+    waitpid(childPID, NULL, 0);
+    
+    println("Sou o pai, pid do filho: " + to_string(childPID));
+}
+
+void task2(){
+    commandToRun = 3;
+    int childPID = fork();
+
+    // Fork do filho
+    if (childPID == 0) 
+        task2Child(); 
+    // Fork do pai 
+    else 
+        task2Father(childPID);
+}
+
+////////////////////////////////////////////////////////////////////////////
+//  HANDLERS
+////////////////////////////////////////////////////////////////////////////
+void handlerTask1 (int sigNumber){
     if (sigNumber == SIGUSR1){
         println("\nSIGUSR1");
         print("\nEsperando comando");
     }
 }
 
-void task2 (int sigNumber){
+void handlerTask2 (int sigNumber){
     if (sigNumber == SIGUSR2) {
         println("\nSIGUSR2");
+        task2();
         print("\nEsperando comando");
     }
 }
 
-void finishTask (int sigNumber){
+void handlerFinishTask (int sigNumber){
     if (sigNumber == SIGTERM) {
         println("\nFinalizando o disparador");
         sleep(1);
@@ -52,27 +122,34 @@ void finishTask (int sigNumber){
     }
 }
 
+////////////////////////////////////////////////////////////////////////////
+//  FUNÇÃO PRINCIPAL
+////////////////////////////////////////////////////////////////////////////
 int main(){
-    //int comandoParaExecutar = 0;
-    
+    // Mostra o valor de pid do processo atual
     showPID();
 
-    if (signal(SIGUSR1, task1) == SIG_ERR)
+    // Inicializa zerada a variavel de comandos
+    commandToRun = 0;
+
+    // Configura o handler da task 1 (verificando erros)
+    if (signal(SIGUSR1, handlerTask1) == SIG_ERR)
         println("-> Erro de tratamento do SIGUSR1");
 
-    if (signal(SIGUSR2, task2) == SIG_ERR)
+    // Configura o handler da task 2 (verificando erros)
+    if (signal(SIGUSR2, handlerTask2) == SIG_ERR)
         println("-> Erro de tratamento do SIGUSR2");
         
-    if (signal(SIGTERM, finishTask) == SIG_ERR)
+    // Configura o handler de finalização do processo (verificando erros)
+    if (signal(SIGTERM, handlerFinishTask) == SIG_ERR)
         println("-> Erro de tratamento do SIGTERM");
     
+    // Loop infinito
     print("Esperando comando");
     while(true){
         print(".");
         sleep(1);
     }
-    
-    println("-> Codigo finalizado com sucesso");
 
     return 0;
 }
